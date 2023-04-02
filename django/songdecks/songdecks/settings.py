94193e916b61d5e44403cab96342f72474c6faa0
NathanielHart44 <nathaniel.hart@gmail.com>
2023-04-02 17:28:20 -0600

    db stuff

--- django/songdecks/songdecks/settings.py
+++ django/songdecks/songdecks/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)r-zn)w609yn&&b70@l6k64q9+l6c94t6j#ju(b3c%lga@d7x)'
+SECRET_KEY = env('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
