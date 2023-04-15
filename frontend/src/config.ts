7df1c63a6232c11e0782a98ef0e8e680e841e22c
NathanielHart44 <nathaniel.hart@gmail.com>
2023-04-15 17:18:11 -0600

    host api fix(?)

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -4,7 +4,8 @@ import { PATH_PAGE } from './routes/paths';
 // API
 // ----------------------------------------------------------------------
 
-export const HOST_API = process.env.REACT_APP_HOST_API_KEY || '';
+// export const HOST_API = process.env.REACT_APP_HOST_API_KEY || '';
+export const HOST_API = '';
 
 export const MAIN_API = {
   // base_url: "http://localhost:8080/",
