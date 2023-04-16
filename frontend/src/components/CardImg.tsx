d7801a70047e6d7cc873ff7605c13d4bdd99e436
NathanielHart44 <nathaniel.hart@gmail.com>
2023-04-15 19:37:59 -0600

    removed unneeded import

--- frontend/src/components/CardImg.tsx
+++ frontend/src/components/CardImg.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography, useTheme } from "@mui/material";
+import { Box, Stack, useTheme } from "@mui/material";
 import Iconify from "./Iconify";
 
 // ----------------------------------------------------------------------
