c959ed8b021acf33734e1c9c0c3d40cb278233e2
NathanielHart44 <nathaniel.hart@gmail.com>
2023-06-01 21:41:00 -0600

    url change

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -9,7 +9,7 @@ export const HOST_API = '';
 
 export const MAIN_API = {
   // base_url: "http://localhost:8080/",
-  base_url: "http://ec2-3-22-171-27.us-east-2.compute.amazonaws.com:8080/",
+  base_url: "https://asoiaf-decks.com:8080/",
 };
 
 // ROOT PATH AFTER LOGIN SUCCESSFUL
