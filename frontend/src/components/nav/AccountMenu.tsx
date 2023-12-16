2283eecaf97204ac7bc946cd96afc5c49d075b40
NathanielHart44 <nathaniel.hart@gmail.com>
2023-12-15 21:57:13 -0700

    frontend started

---
+++ frontend/src/components/nav/AccountMenu.tsx
@@ -0,0 +1,193 @@
+import { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Box, Avatar, Menu, MenuItem, Divider, IconButton, Typography, ListItemIcon, useTheme } from '@mui/material';
+import { MetadataContext } from 'src/contexts/MetadataContext';
+import { logout } from 'src/utils/jwt';
+import { PATH_PAGE } from 'src/routes/paths';
+import { User } from 'src/@types/types';
+import Iconify from '../base/Iconify';
+
+// icons
+import HomeIcon from '@mui/icons-material/Home';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+import HelpIcon from '@mui/icons-material/Help';
+import ConstructionIcon from '@mui/icons-material/Construction';
+
+// ----------------------------------------------------------------------
+
+export default function AccountMenu() {
+
+    const { isMobile, currentUser } = useContext(MetadataContext);
+    const is_moderator = currentUser?.moderator;
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const navigate = useNavigate();
+    const theme = useTheme();
+
+    const handleClick = (event: any) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = (type: string) => {
+        switch (type) {
+            case 'home':
+                navigate(PATH_PAGE.home);
+                break;
+            case 'workbench':
+                navigate(PATH_PAGE.workbench);
+                break;
+            case 'manage_content':
+                navigate(PATH_PAGE.manage);
+                break;
+            case 'moderator':
+                navigate(PATH_PAGE.moderator)
+                break;
+            case 'profile':
+                // navigate(`/profile/${currentUser?.id}`);
+                break;
+            case 'logout':
+                logout();
+                break;
+            default:
+                break;
+        }
+        setAnchorEl(null);
+    };
+
+    return (
+        <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+            <IconButton
+                onClick={handleClick}
+                size="small"
+                sx={{ ml: 2 }}
+                aria-controls={open ? 'account-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+            >
+                <Iconify
+                    icon={'eva:menu-outline'}
+                    color={theme.palette.text.primary}
+                    width={30}
+                    height={30}
+                />
+            </IconButton>
+
+            <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={ ()=> handleClose('default') }
+                PaperProps={{
+                    elevation: 0,
+                    sx: {
+                        overflow: 'visible',
+                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                        mt: 1.5,
+                        ml: isMobile ? 1.5 : 0,
+                        bgcolor: theme.palette.background.paper,
+                        '& .MuiAvatar-root': {
+                            width: 32,
+                            height: 32,
+                            ml: -0.5,
+                            mr: 1,
+                        },
+                        '&:before': {
+                            content: '""',
+                            display: 'block',
+                            position: 'absolute',
+                            top: 0,
+                            right: 15,
+                            width: 10,
+                            height: 10,
+                            bgcolor: 'background.paper',
+                            transform: 'translateY(-50%) rotate(45deg)',
+                            zIndex: 0,
+                        },
+                    },
+                }}
+                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+                <MenuItem onClick={ ()=> handleClose('profile') } disabled>
+                    <ListItemIcon>
+                        <AvatarDisplay is_main={false} currentUser={currentUser} />
+                    </ListItemIcon>
+                    {currentUser ? currentUser.username : '--'}
+                </MenuItem>
+                <MenuItem onClick={ ()=> handleClose('home')} disabled>
+                    <ListItemIcon>
+                        <HelpIcon fontSize="small" />
+                    </ListItemIcon>
+                    How To Use
+                </MenuItem>
+                <Divider />
+                { is_moderator &&
+                    [
+                        <MenuItem key={"moderator"} onClick={ ()=> handleClose('moderator') }>
+                            <ListItemIcon>
+                                <AdminPanelSettingsIcon fontSize="small" />
+                            </ListItemIcon>
+                            Moderators
+                        </MenuItem>,
+                        <MenuItem key={"manage_content"} onClick={ ()=> handleClose('manage_content') }>
+                            <ListItemIcon>
+                                <Settings fontSize="small" />
+                            </ListItemIcon>
+                            Manage Content
+                        </MenuItem>,
+                        <MenuItem key={"workbench"} onClick={ ()=> handleClose('workbench') }>
+                            <ListItemIcon>
+                                <ConstructionIcon fontSize="small" />
+                            </ListItemIcon>
+                            Workbench
+                        </MenuItem>,
+                        <Divider key={"divider"} />
+                    ]
+                }
+                <MenuItem onClick={ ()=> handleClose('home')}>
+                    <ListItemIcon>
+                        <HomeIcon fontSize="small" />
+                    </ListItemIcon>
+                    Home
+                </MenuItem>
+                <MenuItem onClick={ ()=> handleClose('logout') }>
+                    <ListItemIcon>
+                        <Logout fontSize="small" />
+                    </ListItemIcon>
+                    Logout
+                </MenuItem>
+            </Menu>
+        </Box>
+    );
+}
+
+// ----------------------------------------------------------------------
+
+type AvatarDisplayProps = {
+    is_main: boolean;
+    currentUser: User | undefined;
+}
+
+function AvatarDisplay({ is_main, currentUser }: AvatarDisplayProps) {
+    const theme = useTheme();
+    const size = is_main ? 40 : 32;
+
+    return (
+        <Avatar
+            sx={{
+                width: size,
+                height: size,
+                bgcolor: theme.palette.primary.main,
+            }}
+        >
+            <Typography variant={is_main ? "h6" : "body1"} fontFamily={'Metamorphous'} sx={{ color: theme.palette.text.primary }}>
+            {/* <Typography sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: theme.palette.text.primary }}> */}
+                {currentUser ? currentUser.first_name[0].toUpperCase() + currentUser.last_name[0].toUpperCase() : '--'}
+            </Typography>
+        </Avatar>
+    )
+}
---
+++ frontend/src/pages/Workbench.tsx
@@ -0,0 +1,312 @@
+import Page from "src/components/base/Page";
+import axios from "axios";
+import { MAIN_API } from "src/config";
+import { processTokens } from "src/utils/jwt";
+import { useEffect, useState } from "react";
+import { Tag, Proposal, ProposalImage, Task } from "src/@types/types";
+import { useSnackbar } from "notistack";
+import { Accordion, AccordionDetails, AccordionSummary, Box, Button, Card, CardActions, CardContent, CardMedia, Collapse, Container, Grid, IconButton, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, alpha, useTheme } from "@mui/material";
+import LoadingBackdrop from "src/components/base/LoadingBackdrop";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
+// ----------------------------------------------------------------------
+
+export default function Workbench() {
+
+    const { enqueueSnackbar } = useSnackbar();
+
+    const [awaitingResponse, setAwaitingResponse] = useState<boolean>(true);
+    const [allTags, setAllTags] = useState<Tag[]>();
+    const [allProposals, setAllProposals] = useState<Proposal[]>();
+    const [allTasks, setAllTasks] = useState<Task[]>();
+    const [open, setOpen] = useState<boolean>(true);
+
+    const grid_sizing = {
+        xs: 12,
+        sm: 12,
+        md: 12,
+        lg: 6,
+        xl: 4,
+    };
+
+    const getAllContent = async (type: 'tags' | 'proposals' | 'tasks') => {
+        let token = localStorage.getItem('accessToken') ?? '';
+        await axios.get(`${MAIN_API.base_url}get_all_${type}/`, { headers: { Authorization: `JWT ${token}` } }).then((response) => {
+            if (response.status !== 200) {
+                console.log(response);
+                enqueueSnackbar(response.data.detail);
+                return;
+            }
+            if (type === 'tags') { setAllTags(response.data) }
+            if (type === 'proposals') { setAllProposals(response.data) }
+            if (type === 'tasks') { setAllTasks(response.data) }
+        }).catch((error) => {
+            console.error(error);
+        })
+    };
+
+    const updateProposal = async (proposal: Proposal) => {
+        let token = localStorage.getItem('accessToken') ?? '';
+        const formData = new FormData();
+        formData.append('text', proposal.text);
+        // formData.append('images', proposal.images);
+        // formData.append('tags', proposal.tags);
+        formData.append('status', proposal.status);
+        await axios.post(`${MAIN_API.base_url}update_proposal/${proposal.id}/`, formData, { headers: { Authorization: `JWT ${token}` } }).then((response) => {
+            if (response.status !== 200) {
+                console.log(response);
+                enqueueSnackbar(response.data.detail);
+                return;
+            }
+            console.log(response);
+            // take the updated proposal and replace it in the array
+            let updated_proposal = response.data;
+            let updated_proposals = allProposals?.map((proposal) => {
+                if (proposal.id === updated_proposal.id) {
+                    return updated_proposal;
+                }
+                return proposal;
+            });
+            setAllProposals(updated_proposals);
+        }).catch((error) => {
+            console.error(error);
+        })
+    };
+
+    useEffect(() => {
+        processTokens(() => {
+            getAllContent('tags');
+            getAllContent('proposals');
+            getAllContent('tasks');
+        })
+    }, []);
+
+    useEffect(() => {
+        if (allTags && allProposals && allTasks) {
+            setAwaitingResponse(false);
+        }
+    }, [allTags, allProposals, allTasks]);
+
+    return (
+        <Page title="Workbench">
+            <Container maxWidth={false}>
+                { awaitingResponse && <LoadingBackdrop /> }
+                <Stack spacing={2} width={'100%'}>
+                    <AccordionContainer
+                        open={open}
+                        setOpen={setOpen}
+                        allProposals={allProposals}
+                        updateProposal={updateProposal}
+                    />
+                </Stack>
+            </Container>
+        </Page>
+    )
+}
+
+// ----------------------------------------------------------------------
+
+type AccordionContainerType = {
+    open: boolean;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    allProposals: Proposal[] | undefined;
+    updateProposal: (proposal: Proposal) => Promise<void>;
+};
+
+function AccordionContainer({ open, setOpen, allProposals, updateProposal }: AccordionContainerType) {
+
+    const theme = useTheme();
+
+    return (
+        <Accordion
+            disableGutters={true}
+            expanded={open}
+            sx={{ ...(open && { bgcolor: 'transparent' }) }}
+            TransitionProps={{ unmountOnExit: true }}
+        >
+            <AccordianSummaryDiv open={open} setOpen={setOpen} title={"Proposals"} />
+            <AccordionDetails sx={{ pt: 3 }}>
+                <TableContainer>
+                    <Table stickyHeader>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell />
+                                <TableCell align="right">Status</TableCell>
+                                <TableCell align="right">Creator</TableCell>
+                                <TableCell align="right">Created</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody
+                            sx={{
+                                '& > :nth-of-type(2n+1)': { bgcolor: alpha(theme.palette.primary.main, 0.05) },
+                            }}
+                        >
+                            {allProposals && allProposals.map((proposal: Proposal) => (
+                                <ProposalLine
+                                    key={'proposal' + proposal.id}
+                                    proposal={proposal}
+                                    updateProposal={updateProposal}
+                                />
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </AccordionDetails>
+        </Accordion>
+    );
+}
+
+// ----------------------------------------------------------------------
+
+type AccordianSummaryDivType = {
+    open: boolean;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    title: string;
+};
+
+function AccordianSummaryDiv({ open, setOpen, title }: AccordianSummaryDivType) {
+
+    const theme = useTheme();
+
+    const accordian_transition: string = '0.5s background-color;';
+    const text_transition: string = '0.5s color;';
+    const open_background_color = alpha(theme.palette.primary.main, theme.palette.action.selectedOpacity);
+
+    return (
+        <AccordionSummary
+            onClick={() => { setOpen(!open) }}
+            expandIcon={<ExpandMoreIcon />}
+            sx={{
+                borderRadius: '8px',
+                transition: accordian_transition,
+                color: theme.palette.text.secondary,
+                ...(open && { bgcolor: open_background_color }),
+            }}
+        >
+            <Typography
+                sx={{
+                    transition: text_transition,
+                    ...(open && { color: theme.palette.primary.main })
+                }}
+            >
+                {title}
+            </Typography>
+        </AccordionSummary>
+    )
+}
+
+// ----------------------------------------------------------------------
+
+type ProposalLineType = {
+    proposal: Proposal;
+    updateProposal: (proposal: Proposal) => void;
+};
+
+function ProposalLine({ proposal, updateProposal }: ProposalLineType) {
+
+    const [open, setOpen] = useState<boolean>(false);
+    const has_image = false;
+
+    function createTask() {
+        console.log('create task');
+        processTokens(() => {
+            updateProposal({ ...proposal, status: 'confirmed' });
+        })
+        // create Task
+    };
+
+    function rejectTask() {
+        console.log('reject task');
+        processTokens(() => {
+            updateProposal({ ...proposal, status: 'rejected' });
+        })
+    };
+
+    return (
+        <>
+            <TableRow
+                key={'proposal' + proposal.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+                <TableCell component="th" scope="row">
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
+                </TableCell>
+                <TableCell align="right">{proposal.status}</TableCell>
+                <TableCell align="right">
+                    {proposal.creator && proposal.creator.user ? proposal.creator.user.username : 'No Creator'}
+                </TableCell>
+                <TableCell align="right">{proposal.created_at}</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box sx={{ margin: 1 }}>
+                            { has_image &&
+                                <CardMedia
+                                    component="img"
+                                    height={175}
+                                    image={'https://i.natgeofe.com/n/ac577e56-2e49-4723-a422-82b1fca79c3f/green-iguana.jpg?w=1272&h=848'}
+                                    alt="green iguana"
+                                />
+                            }
+                            <Typography
+                                paragraph
+                                variant="body2"
+                                color="text.secondary"
+                            >
+                                {proposal.text}
+                            </Typography>
+                            <Stack direction={'row'} spacing={2} justifyContent={'center'} alignItems={'center'}>
+                                <Button
+                                    variant={"contained"}
+                                    onClick={createTask}
+                                >
+                                    Create Task
+                                </Button>
+                                <Button
+                                    color={"secondary"}
+                                    variant={"contained"}
+                                    onClick={rejectTask}
+                                >
+                                    Reject
+                                </Button>
+                            </Stack>
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </>
+    )
+}
+
+// ----------------------------------------------------------------------
+
+type StatusTagType = {
+    status: 'pending' | 'rejected' | 'closed' | 'confirmed';
+};
+
+function StatusTag({ status }: StatusTagType) {
+
+    const theme = useTheme();
+
+    return (
+        <Typography
+            sx={{
+                color: theme.palette.text.secondary,
+                bgcolor: theme.palette.action.selected,
+                borderRadius: '8px',
+                padding: '0.25rem 0.5rem',
+            }}
+        >
+            {status}
+        </Typography>
+    )
+}
