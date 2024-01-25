d8a56191a81458c9940383fd6e7a4a8b9d78b239
NathanielHart44 <nathaniel.hart@gmail.com>
2024-01-25 09:10:56 -0700

    re-did the landing page with carousel

---
+++ frontend/src/components/carousel/AppFeatured.tsx
@@ -0,0 +1,152 @@
+import Slider from 'react-slick';
+import { m } from 'framer-motion';
+import { useState, useRef } from 'react';
+// @mui
+import { alpha, useTheme, styled } from '@mui/material/styles';
+import { CardContent, Box, Card, Typography, CardMedia } from '@mui/material';
+// components
+
+import { LandingPageInfo } from 'src/pages/LandingPage';
+import CarouselArrows from './CarouselArrows';
+import CarouselDots from './CarouselDots';
+import MotionContainer from '../animate/MotionContainer';
+import { varFade } from '../animate/fade';
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+// ----------------------------------------------------------------------
+
+const OverlayStyle = styled('div')(({ theme }) => ({
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  zIndex: 8,
+  position: 'absolute',
+  backgroundColor: alpha(theme.palette.grey[900], 0.64),
+}));
+
+// ----------------------------------------------------------------------
+
+type Props = {
+  items: LandingPageInfo[];
+}
+
+export default function AppFeatured({ items }: Props) {
+  const theme = useTheme();
+  const carouselRef = useRef<Slider>(null);
+  const [currentIndex, setCurrentIndex] = useState(
+    theme.direction === 'rtl' ? items.length - 1 : 0
+  );
+
+  const settings = {
+    autoPlaySpeed: 5000,
+    autoplay: true,
+    speed: 1500,
+    dots: true,
+    arrows: false,
+    fade: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    rtl: Boolean(theme.direction === 'rtl'),
+    beforeChange: (current: number, next: number) => setCurrentIndex(next),
+    ...CarouselDots({
+      zIndex: 9,
+      top: 24,
+      left: 24,
+      position: 'absolute',
+    }),
+  };
+
+  const handlePrevious = () => {
+    carouselRef.current?.slickPrev();
+  };
+
+  const handleNext = () => {
+    carouselRef.current?.slickNext();
+  };
+
+  return (
+    <Card sx={{ width: '100%' }}>
+
+      <Slider ref={carouselRef} {...settings}>
+        {items.map((item, index) => (
+          <CarouselItem key={item.title + `_${index}`} item={item} isActive={index === currentIndex} />
+        ))}
+      </Slider>
+
+      <CarouselArrows
+        onNext={handleNext}
+        onPrevious={handlePrevious}
+        spacing={0}
+        sx={{
+          top: 16,
+          right: 16,
+          position: 'absolute',
+          '& .arrow': {
+            p: 0,
+            width: 32,
+            height: 32,
+            opacity: 0.48,
+            color: 'common.white',
+            '&:hover': { color: 'common.white', opacity: 1 },
+          },
+        }}
+      />
+    </Card>
+  );
+}
+
+// ----------------------------------------------------------------------
+
+type CarouselItemProps = {
+  item: LandingPageInfo;
+  isActive?: boolean;
+};
+
+function CarouselItem({ item, isActive }: CarouselItemProps) {
+  const { title, text, image } = item;
+
+  return (
+    <Box sx={{ position: 'relative', width: '100%', height: '100%' }}>
+      {image[0] ?
+        <CardMedia
+          component="img"
+          image={image[0]}
+          alt={title}
+          sx={{
+            height: { xs: 280, xl: 320 },
+            objectFit: 'cover',
+          }}
+        /> :
+        <Box sx={{ height: { xs: 280, xl: 320 } }} />
+      }
+      <CardContent
+        component={MotionContainer}
+        animate={isActive}
+        action
+        sx={{
+          bottom: 0,
+          width: '100%',
+          zIndex: 9,
+          textAlign: 'left',
+          position: 'absolute',
+          backgroundColor: (theme) => alpha(theme.palette.grey[900], 0.72),
+        }}
+      >
+        {/* <m.div variants={varFade().inRight}> */}
+          <Typography variant="h5" gutterBottom>
+            {title}
+          </Typography>
+        {/* </m.div> */}
+        {/* <m.div variants={varFade().inRight}> */}
+          <Typography variant="body2">
+            {text}
+          </Typography>
+        {/* </m.div> */}
+      </CardContent>
+      <OverlayStyle />
+    </Box>
+  );
+}
--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,18 +1,26 @@
-import { Box, Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Dialog, DialogContent, Divider, Stack, Typography, useTheme } from '@mui/material';
-import { useContext, useState } from 'react';
-import { MetadataContext } from 'src/contexts/MetadataContext';
+import { Button, Container, Divider, Stack, Typography, useTheme } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { PATH_AUTH } from 'src/routes/paths';
 import Page from 'src/components/base/Page';
 import { MAIN_API } from 'src/config';
+import AppFeatured from 'src/components/carousel/AppFeatured';
+
+// ----------------------------------------------------------------------
+
+export type LandingPageInfo = {
+    title: string,
+    text: string,
+    image: string[],
+};
 
 // ----------------------------------------------------------------------
 
 export default function LandingPage() {
 
     const theme = useTheme();
+    const navigate = useNavigate();
 
-    const info = [
+    const info: LandingPageInfo[] = [
         {
           title: 'Manage Your Tactics Deck From The Browser',
           text: 'No more forgetting your cards at home!',
@@ -42,151 +50,37 @@ export default function LandingPage() {
           title: 'And Much More to Come!',
           text: 'We\'re still working to add new features and make this a useful tool for the community. We want your feedback on what to build next!',
           image: [],
-        },
-        {
-          title: 'Sign Up',
-          text: 'Create an account and get started!',
-          image: [],
         }
     ];
 
     return (
         <Page title="Home">
-            <Stack spacing={6} alignItems={'center'} justifyContent={'center'} sx={{ width: '100%', height: '100%' }}>
-                <Stack spacing={1} alignItems={'center'} justifyContent={'center'} sx={{ width: '100%' }}>
-                    <Typography variant={'h3'} paragraph sx={{ textAlign: 'center', mb: 0 }}>
-                        Digital Tactics Deck
-                    </Typography>
-                    <Stack alignItems={'center'} justifyContent={'center'} sx={{ width: '50%' }}>
-                        <Divider flexItem />
+            <Container maxWidth={'sm'}>
+                <Stack spacing={6} alignItems={'center'} justifyContent={'center'} sx={{ width: '100%', height: '100%' }}>
+                    <Stack spacing={1} alignItems={'center'} justifyContent={'center'} sx={{ width: '100%' }}>
+                        <Typography variant={'h3'} paragraph sx={{ textAlign: 'center', mb: 0 }}>
+                            Digital Tactics Deck
+                        </Typography>
+                        <Stack alignItems={'center'} justifyContent={'center'} sx={{ width: '50%' }}>
+                            <Divider flexItem />
+                        </Stack>
+                        <Typography paragraph color={theme.palette.text.secondary} sx={{ textAlign: 'center', mb: 0 }}>
+                            The tactics deck management tool for the ASOIAF miniatures game.
+                        </Typography>
                     </Stack>
-                    <Typography paragraph color={theme.palette.text.secondary} sx={{ textAlign: 'center', mb: 0 }}>
-                        The tactics deck management tool for the ASOIAF miniatures game.
-                    </Typography>
-                </Stack>
 
-                <Stack
-                    spacing={6}
-                    alignItems={'center'}
-                    sx={{ width: '100%', height: '100%' }}
-                >
-                    {info.map((info_part, index) => (
-                        <ExCard key={index} info_part={info_part} />
-                    ))}
+                    <AppFeatured items={info} />
+
+                    <Stack spacing={2} alignItems={'center'} justifyContent={'center'} sx={{ width: '100%' }}>
+                        <Typography paragraph color={theme.palette.text.secondary} sx={{ textAlign: 'center', mb: 0 }}>
+                            Create an account and get started!
+                        </Typography>
+                        <Button variant={'contained'} fullWidth onClick={() => { navigate(PATH_AUTH.register) }}>
+                            Get Started
+                        </Button>
+                    </Stack>
                 </Stack>
-            </Stack>
+            </Container>
         </Page>
     );
 };
-
-// ----------------------------------------------------------------------
-
-type ExCardProps = {
-    info_part: {
-        title: string,
-        text: string,
-        image: string[],
-    }
-}
-
-function ExCard({ info_part }: ExCardProps) {
-
-    const { isMobile } = useContext(MetadataContext);
-    const theme = useTheme();
-    const navigate = useNavigate();
-    const has_img = info_part.image.length > 0;
-
-    const [popUpOpen, setPopUpOpen] = useState(false);
-
-    const handleClick = () => { setPopUpOpen(!popUpOpen) };
-
-    return (
-        <Card sx={{ maxHeight: 530, width: isMobile ? '80%' : '50%' }}>
-            <CardActionArea onClick={handleClick} sx={{ maxHeight: 480 }} disabled={!has_img}>
-                { has_img &&
-                    <CardMedia
-                        component="img"
-                        image={info_part.image[0]}
-                        alt={info_part.title}
-                        sx={{
-                            height: 300,
-                            objectFit: 'cover',
-                        }}
-                    />
-                }
-                <CardContent sx={{ maxHeight: info_part.title === 'Sign Up' ? 110 : 200 }}>
-                    { info_part.title !== 'BUTTON' &&
-                        <Typography variant={'h6'}>
-                            {info_part.title}
-                        </Typography>
-                    }
-                    <Typography paragraph variant="body2" color={theme.palette.text.secondary}>
-                        {info_part.text}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-            { info_part.title === 'Sign Up' &&
-                <CardActions sx={{ maxHeight: 50 }}>
-                    <Button variant={'contained'} fullWidth onClick={() => { navigate(PATH_AUTH.register) }}>
-                        Get Started
-                    </Button>
-                </CardActions>
-            }
-
-            <Popup
-                image={info_part.image[0]}
-                open={popUpOpen}
-                handleClose={handleClick}
-            />
-        </Card>
-    )
-}
-
-type PopupProps = {
-    image: string;
-    open: boolean;
-    handleClose: () => void;
-};
-  
-export function Popup({ image, open, handleClose }: PopupProps) {
-  
-    return (
-        <Dialog
-            open={open}
-            onClose={handleClose}
-            maxWidth={'lg'}
-            fullWidth
-            sx={{
-                '& .MuiDialog-paper': {
-                    // bgcolor: 'transparent',
-                    boxShadow: 'none',
-                },
-            }}
-        >
-            <DialogContent onClick={handleClose}>
-                <Box
-                    sx={{
-                    width: '100%',
-                    height: '100%',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    borderRadius: '6px',
-                    overflow: 'hidden',
-                    }}
-                >
-                    <img
-                        src={image}
-                        alt={'Example'}
-                        style={{
-                            width: '100%',
-                            maxHeight: '100vh',
-                            objectFit: 'contain',
-                        }}
-                        loading="eager"
-                    />
-                </Box>
-            </DialogContent>
-        </Dialog>
-    )
-}
